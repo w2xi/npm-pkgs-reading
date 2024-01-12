0194ade86ac84763159be2a9e5f0aaf699d38b80
w2xi <43wangxi@gmail.com>
2024-01-12 13:15:18 +0800

    test

--- yocto-queue/test/queue.test.ts
+++ yocto-queue/test/queue.test.ts
@@ -9,34 +9,33 @@ describe('Queue', () => {
   });
 
   test('should enqueue a value', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    expect(queue.size).toBe(3);
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
+    expect(queue.size).toBe(2);
   });
 
   test('should dequeue a value', () => {
     queue.clear();
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    expect(queue.dequeue()).toBe(1);
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
+    expect(queue.dequeue()).toBe('🐮🐴');
     expect(queue.size).toBe(2);
   });
 
   test('should clear the queue', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
     queue.clear();
     expect(queue.size).toBe(0);
   });
 
   test('should iterate over the values in the queue', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
+    queue.enqueue('🐮🐴');
     const values = [...queue];
-    expect(values).toEqual([1, 2, 3]);
+    expect(values).toEqual(['🐮🐴', '🐮🐴', '🐮🐴']);
   });
 });
